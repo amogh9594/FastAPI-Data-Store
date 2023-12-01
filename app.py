3580d4e71087cb03858b4e521d3039b62f486c25
Amogh Kawle <42133366+amogh9594@users.noreply.github.com>
2023-12-01 18:54:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,98 +1,85 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
-from flask_jwt import JWT, jwt_required
-from flask_sqlalchemy import SQLAlchemy
-from security import authenticate, identity
+from fastapi import FastAPI, HTTPException
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'  
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'write_your_secret_key_here'
-api = Api(app)
-db = SQLAlchemy(app)
+app = FastAPI()
 
-jwt = JWT(app, authenticate, identity) 
+# In-memory data structure to simulate storage
+fake_db = []
+unique_id_counter = 1  # Initialize a counter for assigning unique IDs
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/store-data")
+async def store_data():
+    return {"message": "store_data"}
+
+@app.post("/store-data/{item}")
+async def store_data(item: str):
+    global unique_id_counter
+    
+    # Assign a unique ID to each piece of data
+    data = {"id": unique_id_counter, "item": item}
+    
+    # Increment the unique ID counter
+    unique_id_counter += 1
+    
+    # Simulate storing data in the "database"
+    fake_db.append(data)
+    
+    # Return a successful response
+    return {"message": "Data stored successfully", "data": data}
 
 
-class UserModel(db.Model):
-    __tablename__ = 'users'
+@app.delete("/store-data/{item_id}")
+async def delete_data(item_id: int):
+    # Check if the item with the specified ID exists in the "database"
+    for data in fake_db:
+        if data["id"] == item_id:
+            # Remove the item
+            fake_db.remove(data)
+            return {"message": f"Data deleted successfully for item with ID: {item_id}"}
+    
+    # Return 404 Not Found if the item doesn't exist
+    raise HTTPException(status_code=404, detail=f"Item with ID '{item_id}' not found")
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(80), nullable=False)
 
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
+@app.get("/store-data/{item_id}")
+async def retrieve_data(item_id: int):
+    # Check if the item with the specified ID exists in the "database"
+    for data in fake_db:
+        if data["id"] == item_id:
+            # Return the item
+            return {"message": f"Data retrieved successfully for item with ID: {item_id}", "data": data}
+    
+    # Return 404 Not Found if the item doesn't exist
+    raise HTTPException(status_code=404, detail=f"Item with ID '{item_id}' not found")
 
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+@app.put("/store-data/{item_id}")
+async def update_data(item_id: int, updated_item: str):
+    # Check if the item with the specified ID exists in the "database"
+    for data in fake_db:
+        if data["id"] == item_id:
+            # Update the item
+            data["item"] = updated_item
+            return {"message": f"Data updated successfully for item with ID: {item_id}", "data": data}
 
-    @classmethod
-    def find_by_username(cls, username):
-        return cls.query.filter_by(username=username).first()
+@app.post("/welcome")
+async def welcome(payload: dict):
+    global unique_id_counter
+    
+    # Check if the payload contains a "name" key
+    if "name" not in payload:
+        raise HTTPException(status_code=422, detail="Name not provided in the payload")
 
-    @classmethod
-    def find_by_id(cls, _id):
-        return cls.query.filter_by(id=_id).first()
-
-# Create tables before the first request is handled
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-# Resource to interact with the API and database
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('data', type=str, required=True, help='This field cannot be left blank')
-
-    @jwt_required()
-    def get(self, _id):
-        # Retrieve data from the database using the unique identifier (id)
-        item = UserModel.find_by_id(_id)
-        if item:
-            return {'item': {'id': item.id, 'username': item.username, 'password': item.password}}
-        return {'message': 'Item not found'}, 404
-
-    @jwt_required()
-    def post(self, _id):
-        # Store data in the database with a unique identifier (id) and user's name
-        data = Item.parser.parse_args()
-        if UserModel.find_by_id(_id):
-            return {'message': f'Item with id {_id} already exists.'}, 400
-
-        user = UserModel(id=_id, username=f'User {_id}', password=data['data'])
-        user.save_to_db()
-        return {'message': f'Welcome, {user.username}! Item created successfully.'}, 201
-
-    @jwt_required()
-    def put(self, _id):
-        # Edit and replace existing data in the database using the unique identifier (id)
-        data = Item.parser.parse_args()
-        item = UserModel.find_by_id(_id)
-
-        if item:
-            item.password = data['data']
-            item.save_to_db()
-            return {'message': 'Item updated successfully.'}
-        else:
-            return {'message': 'Item not found'}, 404
-
-    @jwt_required()
-    def delete(self, _id):
-        # Delete data from the database using the unique identifier (id)
-        item = UserModel.find_by_id(_id)
-        if item:
-            db.session.delete(item)
-            db.session.commit()
-            return {'message': 'Item deleted successfully.'}
-        else:
-            return {'message': 'Item not found'}, 404
-
-# Endpoint for interacting with the API
-api.add_resource(Item, '/item/<int:_id>')
-
-if __name__ == '__main__':
-    db.init_app(app)
-    app.run(debug=True)
+    # Assign a unique ID to each welcome message
+    welcome_data = {"id": unique_id_counter, "name": payload["name"]}
+    
+    # Increment the unique ID counter
+    unique_id_counter += 1
+    
+    # Save structured data, including the name
+    fake_db.append(welcome_data)
+    
+    # Return a welcome message with the name
+    return {"message": f"Welcome, {payload['name']}!", "data": welcome_data}
